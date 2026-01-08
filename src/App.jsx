262a31e70f583df73ea40f692fcbbe5b3a2389fa
Raziya-023 <raziaa0023@gmail.com>
2026-01-08 20:43:13 +0530

    Initial setup: Project creation

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Task App Setup Complete!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
